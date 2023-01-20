20cb3191c699c3e5e432dcac6a08e180ab9a3976
estuardo-sazo <estuardosazo1206@gmail.com>
2023-01-19 22:33:47 -0600

    Creacion de estrategia para logion y funcion para generar token

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,20 +1,31 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from 'src/users/services/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { use } from 'passport';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validatUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
-    if (!user) {
-      return null;
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-      return null;
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        const { password, ...rpt } = user;
+        return rpt;
+      }
     }
+    return null;
+  }
+
+  generateJWT(user: User) {
+    const payload = { role: user.role, sub: user.id };
   }
 }
